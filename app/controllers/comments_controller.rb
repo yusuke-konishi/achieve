70d64098acb7840e010f156b286647d275153fa6
Yusuke Dev <ysk.kns.dev@gmail.com>
2017-02-26 01:08:54 +0900

    DIVE15-2: added comment feature with ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,8 +6,11 @@ class CommentsController < ApplicationController
     respond_to do |format|
       if @comment.save
         format.html { redirect_to blog_path(@blog), notice: 'コメントを投稿しました。' }
+        format.json { render :show, status: :created, location: @comment }
+        format.js { render :index }
       else
         format.html { render :new }
+        format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
