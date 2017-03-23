6d152648c3a0970cff13e4364c33fdb5db6f81c8
Yusuke Dev <ysk.kns.dev@gmail.com>
2017-03-24 08:14:22 +0900

    DIVE20-3: change production from heroku to aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'still-badlands-10331.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: '54.65.13.181' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
